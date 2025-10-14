59e2af78101b83e43daa5498fefbbf8d53083d76
RubyMR <ruby.montes.rosas@gmail.com>
2025-10-14 17:31:00 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cuarto_repo',
+    version='0.1.0',
+    packages=find_packages(),
+    description="Un paquete pip siempre de saludo",
+    author='Ruby Montes',
+    author_email='ruby.montes.rosas@ejemplo.com',
+    url='https://github.com/GeraldiRubyMontesRosas/miCuartoRepo2#',
+)
